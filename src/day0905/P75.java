e2920796f73d4e7319f365a5e0820bb22e33e749
LeeJiHun1 <dlwlgnsaaa@naver.com>
2023-09-05 09:53:55 +0900

    20230905

--- src/day0905/P75.java
+++ src/day0905/P75.java
@@ -1,12 +1,25 @@
 package day0905;        //연속 부분 수열 합의 개수
 
+import java.util.HashSet;
+
 public class P75 {
     public static void main(String[] args) {
-
+        int[] elements = {7, 9, 1, 1, 4};
+        System.out.println(solution(elements));
     }
 
     public static int solution(int[] elements) {
-        int answer = 0;
-        return answer;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+                int sum = 0;
+                for (int k = j; k < j + i; k++) {
+                    sum += elements[k % elements.length];
+                }
+                set.add(sum);
+            }
+        }
+
+        return set.size();
     }
 }
